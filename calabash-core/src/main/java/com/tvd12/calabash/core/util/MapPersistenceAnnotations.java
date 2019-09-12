df23ca852e5cb7fc4efa2e744aaa5a3983a97649
Dung Ta Van <itprono3@gmail.com>
2019-09-12 23:33:26 +0700

    rename

--- calabash-core/src/main/java/com/tvd12/calabash/core/util/MapPersistenceAnnotations.java
+++ calabash-core/src/main/java/com/tvd12/calabash/core/util/MapPersistenceAnnotations.java
@@ -1,9 +1,9 @@
 package com.tvd12.calabash.core.util;
 
 import static com.tvd12.ezyfox.reflect.EzyClasses.getVariableName;
-import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 import com.tvd12.calabash.core.annotation.MapPersistence;
+import static com.tvd12.ezyfox.io.EzyStrings.isNoContent;
 
 public final class MapPersistenceAnnotations {
 
@@ -17,7 +17,7 @@ public final class MapPersistenceAnnotations {
 	public static String getMapName(Class<?> clazz) {
 		MapPersistence anno = clazz.getAnnotation(MapPersistence.class);
 		String name = anno.value();
-		return isEmpty(name) ? getVariableName(clazz) : name;
+		return isNoContent(name) ? getVariableName(clazz) : name;
 	}
 	
 }
